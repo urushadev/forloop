3c88233a553b42e068ba1aa3b6a2934e240918ae
Urusha <urusha2517@gmail.com>
2024-11-25 01:48:07 -0500

    ForLoop

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+        int n = 1000;
+
+        for (int i = 1; i <= n; ++i) {
+
+            sum += i;
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
